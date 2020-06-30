e46a95c60c83adbff048f949dcc99507b09c5f67
Mustafamemon <musatafamemon490@gmail.com>
2020-06-30 23:49:15 +0500

    bootstrap added

---
+++ src/DemoPages/editable-label.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Editablelabel extends Component {
+    render() {
+        return (
+            <div>
+                <h5>Hello</h5>
+            </div>
+        );
+    }
+}
+
+export default Editablelabel;
